a92b7c9338195ce63df8b2cfb47c9b362a994768
Alexandra Buzila <alexandra.buzila@gmail.com>
2019-03-17 23:27:41 +0100

    Refactor duplicate code from main class

--- src/com/iopo/Main.java
+++ src/com/iopo/Main.java
@@ -1,74 +1,52 @@
 package com.iopo;
 
 import java.text.DecimalFormat;
-import java.util.LinkedList;
-import java.util.List;
 
 public class Main {
 
-        public static void main(String[] args) {
+	private static final DecimalFormat df = new DecimalFormat("###.##");
 
-            DecimalFormat df = new DecimalFormat("###.##");
+	public static void main(String[] args) {
 
-            Car vehicle1 = new CKlasse("123456789", 45);
+		Car vehicle1 = new CKlasse("123456789", 45);
 
-            List<Double> calculateAverageConsumption = new LinkedList<>();
+		double totalAverage = calculateAverageConsumption(vehicle1);
+		System.out.println("Average consumption for this trip is: " + df.format(totalAverage));
 
-            vehicle1.start();
-            vehicle1.shiftGear(1);
-            calculateAverageConsumption.add(vehicle1.drive(0.1, 2800));
-            vehicle1.shiftGear(2);
-            calculateAverageConsumption.add(vehicle1.drive(0.3, 2900));
-            vehicle1.shiftGear(3);
-            calculateAverageConsumption.add(vehicle1.drive(0.5, 3300));
-            vehicle1.shiftGear(4);
-            calculateAverageConsumption.add(vehicle1.drive(50, 3500));
-            vehicle1.shiftGear(3);
-            calculateAverageConsumption.add(vehicle1.drive(10, 2800));
-            vehicle1.shiftGear(4);
-            calculateAverageConsumption.add(vehicle1.drive(2.00, 3000));
-            vehicle1.shiftGear(5);
-            calculateAverageConsumption.add(vehicle1.drive(20.00, 2800));
-            vehicle1.stop();
+		System.out.println("##########################################");
+		Car vehicle2 = new SKlasse("1334323456", 40);
 
+		totalAverage = calculateAverageConsumption(vehicle2);
+		System.out.println("Average consumption for this trip is: " + df.format(totalAverage));
+	}
 
-            double sum = 0;
-            double totalAverage = 0;
-            for (int i = 0; i < calculateAverageConsumption.size(); i++) {
-                sum += calculateAverageConsumption.get(i);
-                totalAverage = sum / calculateAverageConsumption.size();
-            }
-            System.out.println("Average consumption for this trip is: " + df.format(totalAverage));
+	private static double calculateAverageConsumption(Car vehicle) {
+		double average;
 
+		vehicle.start();
 
-            System.out.println("##########################################");
-            Car vehicle2 = new SKlasse("1334323456", 40);
+		vehicle.shiftGear(1);
+		average = vehicle.drive(0.1, 2800);
+		
+		vehicle.shiftGear(2);
+		average = (average + vehicle.drive(0.3, 2900)) / 2;
+		
+		vehicle.shiftGear(3);
+		average = (average + vehicle.drive(0.5, 3300)) / 2;
+		
+		vehicle.shiftGear(4);
+		average = (average + vehicle.drive(50, 3500)) / 2;
+		
+		vehicle.shiftGear(3);
+		average = (average + vehicle.drive(10, 2800)) / 2;
+		
+		vehicle.shiftGear(4);
+		average = (average + vehicle.drive(2.00, 3000)) / 2;
+		
+		vehicle.shiftGear(5);
+		average = (average + vehicle.drive(20.00, 2800)) / 2;
+		vehicle.stop();
 
-            calculateAverageConsumption = new LinkedList<>();
-
-            vehicle2.start();
-            vehicle2.shiftGear(1);
-            calculateAverageConsumption.add(vehicle2.drive(0.1, 2800));
-            vehicle2.shiftGear(2);
-            calculateAverageConsumption.add(vehicle2.drive(0.3, 2900));
-            vehicle2.shiftGear(3);
-            calculateAverageConsumption.add(vehicle2.drive(0.5, 3300));
-            vehicle2.shiftGear(4);
-            calculateAverageConsumption.add(vehicle2.drive(50, 3500));
-            vehicle2.shiftGear(3);
-            calculateAverageConsumption.add(vehicle2.drive(10, 2800));
-            vehicle2.shiftGear(4);
-            calculateAverageConsumption.add(vehicle2.drive(2.00, 3000));
-            vehicle2.shiftGear(5);
-            calculateAverageConsumption.add(vehicle2.drive(20.00, 2800));
-            vehicle2.stop();
-
-            sum = 0;
-            totalAverage = 0;
-            for (int i = 0; i < calculateAverageConsumption.size(); i++) {
-                sum += calculateAverageConsumption.get(i);
-                totalAverage = sum / calculateAverageConsumption.size();
-            }
-            System.out.println("Average consumption for this trip is: " + df.format(totalAverage));
-        }
+		return average;
+	}
 }
